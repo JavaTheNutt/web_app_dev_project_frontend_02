99fb51f058ac536e4d708186ee56e0159b15dc3e
Joe Wemyss <support@javathenutt.com>
2017-11-09 22:14:33 +0000

    added rudimentary navigation ui

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,12 +1,6 @@
 <template>
   <v-app light>
   <nav-container></nav-container>
-    <v-btn fab color="accent"></v-btn>
-    <v-btn fab color="secondary"></v-btn>
-    <v-btn fab color="warning"></v-btn>
-    <v-btn fab color="error"></v-btn>
-    <v-btn fab color="info"></v-btn>
-    <v-btn fab color="success"></v-btn>
   </v-app>
 </template>
 
--- src/app/widgets/navigation/nav/NavContainer.vue
+++ src/app/widgets/navigation/nav/NavContainer.vue
@@ -1,28 +1,53 @@
 <template>
-  <v-toolbar dark color="primary">
-    <v-toolbar-side-icon></v-toolbar-side-icon>
-    <v-toolbar-title class="white--text">Title</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-btn icon>
-      <v-icon>search</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>apps</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>refresh</v-icon>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>more_vert</v-icon>
-    </v-btn>
-  </v-toolbar>
+  <div>
+    <v-toolbar dark color="primary">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-layout wrap>
+      <v-navigation-drawer
+        temporary
+        v-model="drawer"
+        light
+        absolute
+      >
+        <v-list class="pa-1">
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img src="https://randomuser.me/api/portraits/men/85.jpg"/>
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>John Leider</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+        <v-list class="pt-0" dense>
+          <v-divider></v-divider>
+          <v-list-tile v-for="item in items" :key="item.title" @click="">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+    </v-layout>
+  </div>
 </template>
 <script>
   export default {
     name: 'nav-container',
     data() {
       return {
-        name: 'navigation'
+        name: 'navigation',
+        drawer: false,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ]
       };
     }
   };
