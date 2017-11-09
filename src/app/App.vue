0c37aea36b29dd4cc2445f3fce5a388778815590
Joe Wemyss <support@javathenutt.com>
2017-11-09 21:03:01 +0000

    added color palatte

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,13 +1,20 @@
 <template>
   <v-app light>
   <nav-container></nav-container>
+    <v-btn fab color="accent"></v-btn>
+    <v-btn fab color="secondary"></v-btn>
+    <v-btn fab color="warning"></v-btn>
+    <v-btn fab color="error"></v-btn>
+    <v-btn fab color="info"></v-btn>
+    <v-btn fab color="success"></v-btn>
   </v-app>
 </template>
 
 <script>
   import NavContainer from './widgets/navigation/nav/NavContainer';
   export default {
-    components: {NavContainer},
+    components: {
+      NavContainer},
     data() {
       return {
 
