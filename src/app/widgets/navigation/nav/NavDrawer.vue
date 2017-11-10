3e08894540b0ad30d1b8fa8f2a4c7238c24449c5
Joe Wemyss <support@javathenutt.com>
2017-11-10 21:34:33 +0000

    updated sidebar ui

--- src/app/widgets/navigation/nav/NavDrawer.vue
+++ src/app/widgets/navigation/nav/NavDrawer.vue
@@ -6,17 +6,7 @@
       light
       absolute
     >
-      <v-list class="pa-1">
-        <v-list-tile avatar>
-          <v-list-tile-avatar>
-            <img src="https://randomuser.me/api/portraits/men/85.jpg"/>
-          </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>John Leider</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-      <v-list class="pt-0" dense>
+      <v-list class="pt-0">
         <v-divider></v-divider>
         <v-list-tile v-for="item in items" :key="item.title" @click="redirect(item.route)">
           <v-list-tile-action>
