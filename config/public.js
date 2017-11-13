231e3cd47661b6641b942be4db4041cdeb368f7b
Joe Wemyss <support@javathenutt.com>
2017-11-13 23:45:05 +0000

    fixed another travis related bug

--- config/public.js
+++ config/public.js
@@ -1,18 +1,24 @@
-var privateConfig;
+var privateConfig, backendBaseUrl, firebaseKey;
 if(process.env.NODE_ENV !== 'production'){
   try {
     privateConfig = require('./private');
   } catch (e) {
   }
 }
-export var backendBaseUrl = privateConfig.backendBaseUrl || process.env.BACKEND_BASE_URL;
+if(!privateConfig){
+  backendBaseUrl =  process.env.BACKEND_BASE_URL;
+    firebaseKey =  {
+    apiKey: process.env.FIRBASE_API_KEY,
+    authDomain: process.env.FIRBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIRBASE_DATABASE_URL,
+    projectId: process.env.FIRBASE_PROJECT_ID,
+    storageBucket: process.env.FIRBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIRBASE_MESSAGING_SENDER_ID
+  };
+}else{
+  backendBaseUrl = privateConfig.backendBaseUrl;
+  firebaseKey = privateConfig.firebaseKey;
+}
+export {backendBaseUrl, firebaseKey}
 
-export var firebaseKey = privateConfig.firebaseKey || {
-  apiKey: process.env.FIRBASE_API_KEY,
-  authDomain: process.env.FIRBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIRBASE_DATABASE_URL,
-  projectId: process.env.FIRBASE_PROJECT_ID,
-  storageBucket: process.env.FIRBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIRBASE_MESSAGING_SENDER_ID
-};
 
