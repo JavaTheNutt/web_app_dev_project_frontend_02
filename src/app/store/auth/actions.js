6cc3ba2b7d6bcd37e0d1b17a9befdc146aac802b
Joe Wemyss <support@javathenutt.com>
2017-11-13 20:48:09 +0000

    tested vuex store

--- src/app/store/auth/actions.js
+++ src/app/store/auth/actions.js
@@ -1,6 +1,6 @@
 import types from './types';
 
 export default{
-  [types.actions.logIn]: ({commit}) => commit([types.mutations.SET_LOGGED_IN], {isLoggedIn: true}),
-  [types.actions.logOut]: ({commit}) => commit([types.mutations.SET_LOGGED_IN], {isLoggedIn: false})
+  [types.actions.logIn]: ({commit}) => commit(types.mutations.SET_LOGGED_IN, {isLoggedIn: true}),
+  [types.actions.logOut]: ({commit}) => commit(types.mutations.SET_LOGGED_IN, {isLoggedIn: false})
 };
--- src/app/store/auth/actions.spec.js
+++ src/app/store/auth/actions.spec.js
@@ -1,7 +1,27 @@
+import types from './types';
+import actions from './actions';
+
+const sandbox = sinon.sandbox.create();
 describe('auth actions', () => {
   'use strict';
-  it('should correctly set logged in to true', () => {
-
+  let commitStub;
+  beforeEach(() => {
+    commitStub = sandbox.spy();
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
+  describe('log in', () => {
+    it('should correctly set logged in to true', () => {
+      actions[types.actions.logIn]({commit: commitStub});
+      expect(commitStub).to.be.calledWith(types.mutations.SET_LOGGED_IN,{isLoggedIn:true});
+    });
+  });
+  describe('log out', () => {
+    it('should correctly set logged in to false', () => {
+      actions[types.actions.logOut]({commit: commitStub});
+      expect(commitStub).to.be.calledWith(types.mutations.SET_LOGGED_IN, {isLoggedIn:false});
+    });
   });
 
 });
