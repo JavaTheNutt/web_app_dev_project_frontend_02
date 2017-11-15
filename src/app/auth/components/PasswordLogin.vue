3566a4da9caacbd69e7c19a2ebd40618931e6bf3
Joe Wemyss <support@javathenutt.com>
2017-11-15 13:32:53 +0000

    added flex properties to form

--- src/app/auth/components/PasswordLogin.vue
+++ src/app/auth/components/PasswordLogin.vue
@@ -1,22 +1,24 @@
 <template>
   <form novalidate>
-    <v-text-field
-      name="emailField"
-      label="Enter your email address"
-      v-model="email"
-      type="email"
-      :rules="[validationRules.required, validationRules.emailValidator]"></v-text-field>
-    <v-text-field
-      name="passwordField"
-      label="Enter your Password"
-      hint="At least 6 characters"
-      v-model="password"
-      :type="passwordShown ? 'text' : 'password'"
-      min="6"
-      :append-icon="passwordShown ? 'visibility_off': 'visibility'"
-      :append-icon-cb="()=>(passwordShown = !passwordShown)"
-      :rules="[validationRules.required, validationRules.passwordLength]"></v-text-field>
-    <v-btn color="accent" dark @click="logIn">Click to login</v-btn>
+    <div class="form-flex-container">
+      <div class="form-flex-container--field"><v-text-field
+        name="emailField"
+        label="Enter your email address"
+        v-model="email"
+        type="email"
+        :rules="[validationRules.required, validationRules.emailValidator]"></v-text-field></div>
+      <div class="form-flex-container--field"><v-text-field
+        name="passwordField"
+        label="Enter your Password"
+        hint="At least 6 characters"
+        v-model="password"
+        :type="passwordShown ? 'text' : 'password'"
+        min="6"
+        :append-icon="passwordShown ? 'visibility_off': 'visibility'"
+        :append-icon-cb="()=>(passwordShown = !passwordShown)"
+        :rules="[validationRules.required, validationRules.passwordLength]"></v-text-field></div>
+      <v-btn color="accent" dark @click="logIn">Click to login</v-btn>
+    </div>
   </form>
 </template>
 <script>
@@ -47,3 +49,30 @@
     }
   };
 </script>
+<style scoped>
+  .form-flex-container{
+    display: flex;
+  }
+  @media all and (max-width: 500px) {
+    .form-flex-container {
+      flex-direction: column;
+    }
+    .form-flex-container--field {
+      width: 100%;
+      flex-direction: column;
+    }
+  }
+
+  @media all and (min-width: 500px) {
+    .form-flex-container {
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+    }
+    .form-flex-container--field {
+      margin: 10px;
+      width: 40%;
+    }
+  }
+</style>
