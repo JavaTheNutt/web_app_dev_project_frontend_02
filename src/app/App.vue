c7d21cd8d33739a53547b495e7360a0bd4eb297a
Joe Wemyss <support@javathenutt.com>
2017-11-15 14:36:56 +0000

    created login modal

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,17 +1,28 @@
 <template>
   <v-app light>
     <nav-container></nav-container>
+    <v-btn fab color="accent" @click.stop="loginShown = !loginShown">login</v-btn>
     <router-view></router-view>
+    <v-dialog v-model="loginShown" max-width="500px">
+      <login-card></login-card>
+    </v-dialog>
   </v-app>
 </template>
 
 <script>
   import NavContainer from './widgets/navigation/nav/NavContainer';
   import authTypes from './auth/vuex/types';
+  import LoginCard from './auth/components/LoginCard.vue';
 
   export default {
     components: {
+      LoginCard,
       NavContainer
+    },
+    data(){
+      return{
+        loginShown: false
+      };
     },
     created() {
       this.$store.dispatch(authTypes.actions.registerAuthStateListener);
---
+++ src/app/auth/components/LoginCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card>
+    <login></login>
+  </v-card>
+</template>
+<script>
+  import Login from './Login';
+  export default{
+    name: 'login-card',
+    components: {Login}
+  };
+</script>
