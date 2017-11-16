77692790d16479b348e3708801040ed2f0eea8cb
Joe Wemyss <support@javathenutt.com>
2017-11-16 22:04:46 +0000

    updated profile view

--- src/app/profile/components/Profile.vue
+++ src/app/profile/components/Profile.vue
@@ -1,5 +1,17 @@
 <template>
-  <h1 class="display-2">This is the {{pageName}} page</h1>
+  <v-container fluid>
+    <v-layout>
+      <v-flex>
+        <p class="headline text-xs-center">Welcome to your Profile page</p>
+      </v-flex>
+    </v-layout>
+    <v-layout row justify-center>
+      <v-flex xs12 md8>
+        <p class="body-1">
+          This is your personal profile page. There's not much here right now, but make sure to check back later!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
   export default {
