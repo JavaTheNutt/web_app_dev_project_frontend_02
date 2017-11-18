9a995f387e3c260292bc9b6d6d5b1025812c9251
Joe Wemyss <support@javathenutt.com>
2017-11-18 12:34:47 +0000

    synchronously load env vars

--- src/main.js
+++ src/main.js
@@ -1,25 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+const config = require('../config/public');
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import VeeValidate from 'vee-validate';
 import './stylus/main.styl';
-
 import App from './app/App';
 import router from './router';
 import store from './store';
 import firebase from 'firebase';
-import {firebaseKey, logLevel} from '../config/public';
+
+//import {firebaseKey, logLevel} from '../config/public';
 import http from './app/service/http';
 import * as Logger from 'loglevel';
 require('font-awesome/css/font-awesome.css');
-Logger.setLevel(logLevel);
+Logger.setLevel(config.logLevel);
 
 Vue.use(Vuetify);
 Vue.use(VeeValidate);
 Vue.config.productionTip = false;
 Vue.prototype.$http      = http;
-firebase.initializeApp(firebaseKey);
+firebase.initializeApp(config.firebaseKey);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
