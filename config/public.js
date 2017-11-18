ae923baee40db269311c234c941bcae89ffdb2be
Joe Wemyss <support@javathenutt.com>
2017-11-18 12:30:48 +0000

    improved env var handling

--- config/public.js
+++ config/public.js
@@ -1,13 +1,13 @@
 var privateConfig, backendBaseUrl, firebaseKey;
-if(process.env.NODE_ENV !== 'production'){
-  try {
-    privateConfig = require('./private');
-  } catch (e) {
-  }
+
+try {
+  privateConfig = require('./private');
+} catch (e) {
 }
-if(!privateConfig){
-  backendBaseUrl =  process.env.BACKEND_BASE_URL;
-    firebaseKey =  {
+
+if (!privateConfig) {
+  backendBaseUrl = process.env.BACKEND_BASE_URL;
+  firebaseKey    = {
     apiKey: process.env.FIRBASE_API_KEY,
     authDomain: process.env.FIRBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIRBASE_DATABASE_URL,
@@ -15,11 +15,15 @@ if(!privateConfig){
     storageBucket: process.env.FIRBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIRBASE_MESSAGING_SENDER_ID
   };
-}else{
+} else {
   backendBaseUrl = privateConfig.backendBaseUrl;
-  firebaseKey = privateConfig.firebaseKey;
+  firebaseKey    = privateConfig.firebaseKey;
 }
-var logLevel = process.env.LOG_LEVEL || process.env.NODE_ENV === 'development' ? 'trace': 'silent';
-export {backendBaseUrl, firebaseKey, logLevel}
+var logLevel = process.env.LOG_LEVEL || process.env.NODE_ENV === 'development' ? 'trace' : 'silent';
+module.exports = {
+  backendBaseUrl: backendBaseUrl,
+  firebaseKey: firebaseKey,
+  logLevel: logLevel
+};
 
 
--- src/app/service/http/index.js
+++ src/app/service/http/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import {backendBaseUrl} from '../../../../config/public';
+const config = require('../../../../config/public');
+//import {backendBaseUrl} from '../../../../config/public';
 
 export default axios.create({
-  baseURL: backendBaseUrl
+  baseURL: config.backendBaseUrl
 });
