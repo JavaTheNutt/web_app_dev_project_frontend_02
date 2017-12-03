d3643f1344e0c3b68af88ac3c4e81e2635cc7515
Joe Wemyss <support@javathenutt.com>
2017-12-03 13:40:15 +0000

    added save as default ui to add address form

--- src/app/addAddress/components/AddAddressForm.vue
+++ src/app/addAddress/components/AddAddressForm.vue
@@ -30,26 +30,29 @@
             type="text"
           ></v-text-field>
         </v-flex>
-        <v-flex v-if="hasSingleDefault">
+        <v-flex>
+          <v-layout row>
+            <v-checkbox label="Select new country" v-model="newCountrySelected" color="primary"
+                        :disabled="hasNoDefault"></v-checkbox>
+            <v-checkbox label="Save as default" v-model="saveAsDefault" color="primary"
+                        :disabled="!saveAsDefaultEnabled"></v-checkbox>
+          </v-layout>
+        </v-flex>
+        <v-flex v-if="hasSingleDefault && !showCountrySelect">
           Default Country: {{defaultCountries[0]}}
         </v-flex>
-        <v-flex v-else-if="hasNoDefault">
+        <v-flex v-else-if="showCountrySelect">
           <country-select
-            :countries="fullCountries"
+            :countries="countriesToShow"
             @countryAdded="countrySelected"
           ></country-select>
-        </v-flex>
-        <v-flex v-else>
-          <country-select
-            :countries="defaultCountries"
-            @countryAdded="countrySelected"></country-select>
         </v-flex>
       </v-layout>
     </v-container>
   </form>
 </template>
 <script>
-
+  import * as Logger from 'loglevel';
   import countryList from 'country-list';
   import {mapGetters} from 'vuex';
   import profileTypes from '@/app/profile/vuex/types';
@@ -65,8 +68,10 @@
           line02: '',
           line03: '',
           country: '',
-          fullCountries: countryList().getNames()
-        }
+        },
+        fullCountries: countryList().getNames(),
+        newCountrySelected: false,
+        saveAsDefault: false
       };
     },
     computed: {
@@ -76,6 +81,26 @@
       },
       hasNoDefault() {
         return this.defaultCountries.length === 0;
+      },
+      countriesToShow() {
+        return this.newCountrySelected || this.hasNoDefault ? this.filteredCountries : this.defaultCountries;
+      },
+      showCountrySelect() {
+        return !this.hasSingleDefault || this.newCountrySelected;
+      },
+      filteredCountries() {
+        return this.fullCountries.filter(country => this.defaultCountries.indexOf(country) === -1);
+      },
+      saveAsDefaultEnabled() {
+        return this.hasNoDefault || (!this.hasNoDefault && this.newCountrySelected);
+      }
+    },
+    mounted(){
+      this.saveAsDefault = this.hasNoDefault;
+    },
+    watch: {
+      newCountrySelected(newVal) {
+        this.saveAsDefault = newVal;
       }
     },
     methods: {
